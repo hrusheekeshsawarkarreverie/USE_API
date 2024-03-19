159f30832216ecda25faeda9de7c5e1ca656d66e
hrusheekeshsawarkarreverie <148539120+hrusheekeshsawarkarreverie@users.noreply.github.com>
2024-03-19 12:51:18 +0530

    return embeddings

--- main.py
+++ main.py
@@ -3,23 +3,22 @@ from pydantic import BaseModel
 import pickle
 from model import IntentClassifierAPI
 from typing import List
+
+
+obj=IntentClassifierAPI()
+
+
 app=FastAPI()
 
 class Sentences(BaseModel):
     sentence_list : List[str]
-    intent_list : List[str]
-    num_intents : int
-    input_sentence : str
-
-
 
 @app.post('/')
 async def function(item: Sentences):
     sentence_example=item.sentence_list
-    intent_example=item.intent_list
-    num_different_intents=item.num_intents
-    input_string=item.input_sentence
-    obj=IntentClassifierAPI()
-    obj.run_classifier(sentence_example,intent_example,num_different_intents)
-    output=obj.predict_intent(input_string)
-    return {"prediction":output}
+    # obj=IntentClassifierAPI()
+    encoded_list=obj.run_classifier(sentence_example)
+    print(type(encoded_list))
+    print(encoded_list.shape)
+    print(type(encoded_list.tolist()))
+    return {"Embeddings":encoded_list.tolist()}
